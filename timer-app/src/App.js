e99b5f373d3c0100a0cf9b0bd0c5f916980a00ea
Yerko De Zutter <yerkodezutter@gmail.com>
2019-04-03 09:44:18 +0200

    small make-ups

--- timer-app/src/App.js
+++ timer-app/src/App.js
@@ -57,9 +57,11 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <input type="number" min='1' max='99' name="setMin" id="setMin" value={this.state.min} onChange={this.setC} />
+        <h1>Set timer and countdown</h1>
         <br />
-        {this.state.min.toString().length === 1 ? '0' + this.state.min : this.state.min} : {this.state.sec.toString().length === 1 ? '0' + this.state.sec : this.state.sec}
+        <input type={this.state.button === 'play' ? "number" : "hidden"} min='1' max='99' name="setMin" id="setMin" value={this.state.min} onChange={this.setC} />
+        <br />
+        <h1>{this.state.min.toString().length === 1 ? '0' + this.state.min : this.state.min} : {this.state.sec.toString().length === 1 ? '0' + this.state.sec : this.state.sec}</h1>
         <br />
         <button onClick={this.state.button === 'play' ? this.startC : this.stopC}>{this.state.button}</button>
       </div>
